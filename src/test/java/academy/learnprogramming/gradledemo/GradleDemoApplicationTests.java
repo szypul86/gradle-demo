0b6c769f3ee5c9c646e13b4ce25e567df0a06751
Andrzej Szypulski <szypul86@gmail.com>
2019-11-13 14:02:42 +0100

    gradle excercises

---
+++ src/test/java/academy/learnprogramming/gradledemo/GradleDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.gradledemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
